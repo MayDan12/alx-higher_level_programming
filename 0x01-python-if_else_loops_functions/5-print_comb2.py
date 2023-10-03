0d1f572ea3515b8be3c581d3fee3c8c63ab0b66d
MayDan12 <oluwamayowadan@gmail.com>
2023-10-03 12:17:37 -0700

    5-print_comb2.py

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#maydan daniel
+for The_number in range(0, 100):
+    if The_number == 99:
+        print("{}".format(The_number))
+    else:
+        print("{:02}".format(The_number), end=", ")
