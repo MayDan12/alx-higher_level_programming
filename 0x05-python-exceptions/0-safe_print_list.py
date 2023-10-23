14b3387bb9354fee6abdfaa0678006cf0521f2e3
MayDan12 <oluwamayowadan@gmail.com>
2023-10-23 14:30:45 -0700

    0-correct

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#0-safe_print_list.py
+# 0-safe_print_list.py
 
 def safe_print_list(my_list=[], x=0):
     """This project is a good one"""
     count = 0
-    for i in my_list:
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             count += 1
