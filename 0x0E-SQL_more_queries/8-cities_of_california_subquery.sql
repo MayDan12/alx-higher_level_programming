090c8767bdcf23105d75963a8f39b1b46f7bbdbd
MayDan12 <oluwamayowadan@gmail.com>
2023-12-13 13:53:35 -0800

    8-cities.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,8 +2,11 @@
 -- that can not be found in the database
 USE hbtn_0d_usa;
 
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state_id = states.id
-	AND states.name = 'California'
+SELECT id, name
+FROM cities
+WHERE state_id = (
+	SELECT id
+	FROM states
+	WHERE name = 'California'
+)
 ORDER BY cities.id ASC;
