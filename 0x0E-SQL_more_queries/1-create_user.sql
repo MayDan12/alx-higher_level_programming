ead0f33eea183b3b7cb68830dc500127935a498a
MayDan12 <oluwamayowadan@gmail.com>
2023-12-13 12:46:02 -0800

    1-create_user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creatwes the MySQL server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
