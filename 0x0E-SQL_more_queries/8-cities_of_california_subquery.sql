43f2395b4b93428b656f6a059260eae9d73f4a3a
MayDan12 <oluwamayowadan@gmail.com>
2023-12-13 13:49:48 -0800

    8-cities.sql

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- This is a script that lists all the cities of californa
+-- that can not be found in the database
+USE hbtn_0d_usa;
+
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+	AND states.name = 'California'
+ORDER BY cities.id ASC;
