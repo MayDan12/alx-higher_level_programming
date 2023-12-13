e1fa4ebdf107a08ad921da6cd9828320c1a040fc
MayDan12 <oluwamayowadan@gmail.com>
2023-12-13 12:40:35 -0800

    test

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- This script lists al privilegeds
 -- of trhe MySQL users
-SELECT *
-FROM information_schema.USER_PRIVILEGES
-WHERE GRANTEE = 'user_0d_1@localhost' OR GRANTEE = 'user_0d_2@localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
